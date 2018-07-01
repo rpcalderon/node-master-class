75b1767324f64cd7eb96acbf3c82dd3fd08f29f1
Rosita Calderon <rpcalderon86@gmail.com>
2018-07-01 15:24:11 -0400

    Routing Requests

--- app/index.js
+++ app/index.js
@@ -37,11 +37,42 @@ var server = http.createServer(function(req, res) {
   req.on('end',function(){
     buffer += decoder.end();
 
+    // Choose the chandler this request should go to. If one is not found use the notFound handler
+    var chooseHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    // Contruct the data object to send tot he handler
+    var data = {
+      'trimmedPath' : trimmedPath,
+      'queryStringObject' : queryStringObject,
+      'method' : method,
+      'headers' : headers,
+      'payload' : buffer
+    };
+
+    // Route the request to the handler specified in the router
+    chooseHandler(data,function(statusCode,payload) {
+      // Use the status code called back by the handler, or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+      // Use the payload called back by the handler, or default to an empty object
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      // Convert the payload to a string
+      var payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.writeHeader(statusCode);
+      res.end(payloadString);
+
+      // Log the request path
+      console.log('Returning this response: ', statusCode,payloadString);
+    });
+
     // Send the reponse
-    res.end('Hello World\n');
+    //res.end('Hello World\n');
 
     // Log the request path
-    console.log('Request received with theis payload: ', buffer);
+    //console.log('Request received with theis payload: ', buffer);
   });
 
   // Send the reponse
@@ -56,3 +87,22 @@ var server = http.createServer(function(req, res) {
 server.listen(3000,function(){
   console.log('The server is listening on port 3000 now');
 });
+
+// define the handlers
+var handlers = {};
+
+// Sample Handler
+handlers.sample = function(data,callback) {
+  // Callback a HTTP status code, and a payload object
+  callback(406,{'name': 'same handler'});
+};
+
+// Not found handler
+handlers.notFound = function(data,callback) {
+  callback(404);
+};
+
+// Define a request router
+var router = {
+  'sample': handlers.sample
+};
